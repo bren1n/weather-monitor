8fbb839c5b2d058dbb7209a792a430aca0f6d916
Breno Ferreira <brenoleite1234@gmail.com>
2022-07-17 17:07:14 -0300

    feat: Add map to template and weather change notifications

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
@@ -7,17 +8,21 @@ app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 cities = requests.get('http://127.0.0.1:1026/v2/entities?type=City').json()
-print(cities)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', cities=cities)
 
+
 @app.route('/', methods=['POST'])
 def get_city_data():
     city_id = request.form.get('city_id')
-    city_data = requests.get(f'http://127.0.0.1:1026/v2/entities?type=City&id={city_id}').json()[0]
-    return render_template('index.html', cities=cities, data=city_data)
+    city_data = requests.get(
+        f'http://127.0.0.1:1026/v2/entities?type=City&id={city_id}').json()[0]
+    coordinates = json.dumps(city_data['coordinates']['value'].split(' '))
+    return render_template('index.html', cities=cities, data=city_data, coordinates=coordinates)
+
 
 @app.route('/subscribe', methods=['POST'])
 def publish():
@@ -25,9 +30,11 @@ def publish():
     socketio.send(data, broadcast=True)
     return ('', 204)
 
+
 @socketio.event
 def connect():
-	print('Client connected')
+    print('Client connected')
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0')
